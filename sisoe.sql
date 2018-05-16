ca0aff3cd470a9932e5e5187bb88aceb64a5c213
Blue <jlgpblue@gmail.com>
2018-05-15 20:22:13 -0500

    bd sisoe

--- sisoe.sql
+++ sisoe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-05-2018 a las 00:40:52
+-- Tiempo de generación: 16-05-2018 a las 03:21:21
 -- Versión del servidor: 10.1.25-MariaDB
 -- Versión de PHP: 7.1.7
 
@@ -48,7 +48,8 @@ INSERT INTO `direccion` (`idDireccion`, `Calle`, `NumeroExt`, `NumeroInt`, `Colo
 (0000000001, 'Alcachofa', '256', '', 'El Pitirancho', 'Puerto Vallarta', 'Jalisco', 'Mexico', 48290),
 (0000000002, 'Gladiola', '234', '', 'La Floresta', 'Puerto Vallarta', 'Jalisco', 'Mexico', 34567),
 (0000000003, 'Tulipan', '1270', '', 'La Floresta', 'Puerto Vallarta', 'Jalisco', 'Mexico', 48290),
-(0000000004, 'Azucena', '456', '', 'La Floresta', 'Puerto Vallarta', 'Jalisco', 'Mexico', 48290);
+(0000000004, 'Azucena', '456', '', 'La Floresta', 'Puerto Vallarta', 'Jalisco', 'Mexico', 48290),
+(0000000005, 'Hidalgo', '23', 'A', 'Educacion', 'Puerto Vallarta', 'Jalisco', 'Mexico', 48831);
 
 -- --------------------------------------------------------
 
@@ -66,6 +67,15 @@ CREATE TABLE `eventos` (
   `Asistentes` longtext
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `eventos`
+--
+
+INSERT INTO `eventos` (`idEvento`, `Nombre`, `Tipo`, `Cronograma`, `FechaInicio`, `FechaFin`, `Asistentes`) VALUES
+(1, 'Boda de Juan y Claudia', 'Boda', '<p style=\"text-align: center;\"><strong>Programa de la Boda</strong></p>\r\n<p style=\"text-align: left;\">18:00 Misa en la catedral</p>\r\n<p style=\"text-align: left;\">19:30 Sesion de fotografia</p>\r\n<p style=\"text-align: left;\">20:30 Ingreso al salon y Brindis con los Padrinos</p>\r\n<p style=\"text-align: left;\">21:00 Cena</p>\r\n<p style=\"text-align: left;\">22:00 Apertura del baile</p>\r\n<p style=\"text-align: left;\">01:30 despedida.</p>', '2018-05-16 18:00:00', '2018-05-17 02:00:00', 'Jose Luis Gutierrez Padilla - Padrino\r\nJose Perez Duran - Invitado familiar de la novia\r\nAlberto suarez Rodriguez - Invitado familiar del novio\r\nCristina Acevedo Juarez - Invitada amante del novio\r\nSusana Cervantes - Invitada familiar de la novia'),
+(2, 'Seminario sobre IngenierÃ­a de Software', 'Taller', '<p><strong>Programa de actividades</strong></p>\r\n<p>&nbsp;</p>\r\n<p><em>DIAS MARTES Y JUEVES:</em></p>\r\n<p>15:00 a 16:00 clases teoricas</p>\r\n<p>16:00 a 17:00 practicas de taller</p>\r\n<p>&nbsp;</p>\r\n<p>ULTIMA SEMANA ENTREGA DE PROYECTOS</p>', '2018-01-15 15:00:00', '2018-05-22 15:00:00', 'Participantes:\r\nIng. Sofia Alcazar VÃ©lez - Empresa: Soft-Inc. - telefono de contacto: 3221996588\r\nIng. Sofia Alcazar VÃ©lez - Empresa: Soft-Inc. - telefono de contacto: 3221996588\r\nIng. Sofia Alcazar VÃ©lez - Empresa: Soft-Inc. - telefono de contacto: 3221996588\r\nIng. Sofia Alcazar VÃ©lez - Empresa: Soft-Inc. - telefono de contacto: 3221996588\r\nIng. Sofia Alcazar VÃ©lez - Empresa: Soft-Inc. - telefono de contacto: 3221996588\r\nIng. Sofia Alcazar VÃ©lez - Empresa: Soft-Inc. - telefono de contacto: 3221996588\r\nIng. Sofia Alcazar VÃ©lez - Empresa: Soft-Inc. - telefono de contacto: 3221996588\r\nIng. Sofia Alcazar VÃ©lez - Empresa: Soft-Inc. - telefono de contacto: 3221996588'),
+(3, 'Uso del Algebra Lineal en la ProgramaciÃ³n', 'Curso', '<p><strong>Programa</strong></p>\r\n<p>15:00 - Inicio del Curso y Presentaci&oacute;n.</p>\r\n<p>16:00 - Explicaci&oacute;n del tema.</p>\r\n<p>17:00 - Preguntas y Respuestas.</p>\r\n<p>17:30 - Examen de conocimientos.</p>\r\n<p>18:00 - Fin de Curso y Despedida.</p>', '2018-05-13 15:00:00', '2018-05-15 18:00:00', 'Estudiantes:\r\nAlberto Lopez Suazo\r\nCornelio Rodriguez Lepe\r\nAlejandra Dorantes Loza\r\nHeriberto Machado Duran\r\nGeorgina Lopez Lopez\r\nMiguel Cervantes Duarte\r\nCristian Olvera Prado');
+
 -- --------------------------------------------------------
 
 --
@@ -76,6 +86,15 @@ CREATE TABLE `eventos_has_salones` (
   `Eventos_idEvento` int(11) NOT NULL,
   `Salones_idSalon` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `eventos_has_salones`
+--
+
+INSERT INTO `eventos_has_salones` (`Eventos_idEvento`, `Salones_idSalon`) VALUES
+(1, 1),
+(2, 2),
+(3, 3);
 
 -- --------------------------------------------------------
 
@@ -102,7 +121,8 @@ CREATE TABLE `personas` (
 
 INSERT INTO `personas` (`idPersona`, `Nombres`, `Apellidos`, `Correo`, `Contrasena`, `Empresa`, `Direccion_idDireccion`, `Telefono`, `RFC`, `Tipo`) VALUES
 (1, 'Admin', 'System', 'adminsys@gmail.com', '$2y$10$VWR7ncgAsCX6ae6zWtoOe.FG6OSM655xLQyKCDmg8YFWdnZi6TH4.', 'NidiaSoft', 0000000001, 322115450, '1234567890-EXS', 0),
-(2, 'Jose Luis', 'Gutierrez', 'jose_luis@gmail.com', '$2y$10$z6udqWhxEPICEJ7Xzf0S9ekekCW8/uPbMQST2RrGYJox/dn2bUCnW', 'Universidad del Llano', 0000000004, 322115450, 'GUPL750118-DUO', 1);
+(2, 'Jose Luis', 'Gutierrez', 'jose_luis@gmail.com', '$2y$10$z6udqWhxEPICEJ7Xzf0S9ekekCW8/uPbMQST2RrGYJox/dn2bUCnW', 'Universidad del Llano', 0000000004, 322115450, 'GUPL750118-DUO', 1),
+(3, 'Ernesto Alejandro', 'Zapata Suarez', 'plcom@info.com', '$2y$10$0UPxIipjKooNcvcKeG4yjuJ9xaMFx7Jqokkpj7zxKpVROTF1f/17C', 'Plasticos y Complementos S.A.', 0000000005, 2147483647, 'PLCM920612-MN', 1);
 
 -- --------------------------------------------------------
 
@@ -114,6 +134,15 @@ CREATE TABLE `personas_has_eventos` (
   `Personas_idPersona` int(11) NOT NULL,
   `Eventos_idEvento` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `personas_has_eventos`
+--
+
+INSERT INTO `personas_has_eventos` (`Personas_idPersona`, `Eventos_idEvento`) VALUES
+(2, 1),
+(2, 2),
+(2, 3);
 
 -- --------------------------------------------------------
 
@@ -159,7 +188,7 @@ CREATE TABLE `salones` (
 --
 
 INSERT INTO `salones` (`idSalon`, `Nombre`, `Descripcion`, `Capacidad`, `Tipo`, `imagen`) VALUES
-(1, 'Rubi', 'Salon para eventos recreativos y sociales: aniversarios, bodas, cumpleaÃ±os, XV aÃ±os, bautismos, comuniones, conmemoraciones, graduaciones, reuniones empresariales y universitarias, shows y animaciones.', 140, 'Festivo', '{local}/assets/imagenes/rubi.jpg'),
+(1, 'RubÃ­', 'Salon para eventos recreativos y sociales: aniversarios, bodas, cumpleaÃ±os, XV aÃ±os, bautismos, comuniones, conmemoraciones, graduaciones, reuniones empresariales y universitarias, shows y animaciones.', 140, 'Tipo de Salon', '{local}/assets/imagenes/rubi.jpg'),
 (2, 'Esmeralda', 'Salon para debates, paneles, mesas redondas, talleres y seminarios entre otros.', 80, 'Multiproposito', '{local}/assets/imagenes/esmeralda2.jpg'),
 (3, 'Aguamarina', 'Salon audiovisual para conferencias, simposios, congresos, convenciones, foros y cursos entre otros.', 100, 'Audiovisual', '{local}/assets/imagenes/aguamarina4.jpg');
 
@@ -221,17 +250,17 @@ ALTER TABLE `salones`
 -- AUTO_INCREMENT de la tabla `direccion`
 --
 ALTER TABLE `direccion`
-  MODIFY `idDireccion` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idDireccion` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT de la tabla `eventos`
 --
 ALTER TABLE `eventos`
-  MODIFY `idEvento` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idEvento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `personas`
 --
 ALTER TABLE `personas`
-  MODIFY `idPersona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idPersona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `proveedores`
 --
